65d91f64c1217467eb449e3fa320b376a076d758
xfillmore <xuedan.fillmore@colorado.edu>
2020-12-08 23:35:17 -0700

    updated db setup for added firebase node module

--- firebase.js
+++ firebase.js
@@ -1,4 +1,10 @@
-import firebase from 'firebase'
+// https://css-tricks.com/intro-firebase-react/
+// https://firebase.google.com/docs/firestore/quickstart?authuser=1
+// https://medium.com/technest/how-to-connect-firebase-cloud-firestore-to-your-react-app-1118fd182c60
+
+import * as firebase from 'firebase/app';
+// import firebase from 'firebase';
+import 'firebase/firestore';
 
 var firebaseConfig = {
     apiKey: "AIzaSyBKC3_upjHcGQHA0YQjcptW34BLlFCnTAw",
@@ -10,4 +16,8 @@ var firebaseConfig = {
     appId: "1:809035285720:web:72f15d2da4a582e5b703ad"
 };
 firebase.initializeApp(firebaseConfig);
-export default firebase;
+
+// const firebase = require("firebase");
+// require("firebase/firestore");
+
+export const firebasedb = firebase.firestore();
